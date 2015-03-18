ba8aad58dbf419acaf6ea24538d0b4b21f9a01c3
Saira J. Barlas <c12809080@mydit.ie>
2015-03-18 10:30:12 +0000

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -5,7 +5,7 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-DISPLAY = pygame.display.set_mode((400, 400))
+DISPLAY = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('GAME!')
 while True:
     for event in pygame.event.get():
