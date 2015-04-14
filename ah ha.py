b194219231d67a8e25eca81d73ef395eca0535a5
Saira <c12809080@mydit.ie>
2015-04-14 23:32:27 +0100

    ya

---
+++ ah ha.py
@@ -0,0 +1,11 @@
+import math
+import pygame
+
+screen = pygame.display.set_mode([800, 600])
+pygame.display.set_caption('Breakout')
+pygame.mouse.set_visible(0)
+font = pygame.font.Font(None, 36)
+background = pygame.Surface(screen.get_size())
+
+
+        
