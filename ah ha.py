4c04ae43c456e46ff33be43938e29b98996ec407
Saira <c12809080@mydit.ie>
2015-04-14 23:33:51 +0100

    correction

--- ah ha.py
+++ ah ha.py
@@ -2,7 +2,7 @@ import math
 import pygame
 
 screen = pygame.display.set_mode([800, 600])
-pygame.display.set_caption('Breakout')
+pygame.display.set_caption('Pyblox Game')
 pygame.mouse.set_visible(0)
 font = pygame.font.Font(None, 36)
 background = pygame.Surface(screen.get_size())
