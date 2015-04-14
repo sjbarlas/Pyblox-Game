ad46c2c5d3f45c7393a5e762252b737da5a818a7
Saira <c12809080@mydit.ie>
2015-04-14 23:35:39 +0100

    mmhm

--- ah ha.py
+++ ah ha.py
@@ -1,11 +1,10 @@
 import math
 import pygame
 
-screen = pygame.display.set_mode([800, 600])
+screen = pygame.display.set_mode([800, 800])
 pygame.display.set_caption('Pyblox Game')
 pygame.mouse.set_visible(0)
 font = pygame.font.Font(None, 36)
 background = pygame.Surface(screen.get_size())
 
-
-        
+blocks
