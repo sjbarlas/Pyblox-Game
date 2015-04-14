02ddd1be354a75421fb4d8cd4a6770da5db548d1
Saira <c12809080@mydit.ie>
2015-04-15 00:20:09 +0100

    njh

--- ah ha.py
+++ ah ha.py
@@ -1,90 +1,81 @@
 import math
 import pygame
-
-screen = pygame.display.set_mode([800, 600])
-pygame.display.set_caption('Pyblox Game')
-pygame.mouse.set_visible(0)
-font = pygame.font.Font(None, 36)
-background = pygame.Surface(screen.get_size())
-
-blocks = pygame.sprite.Group()
-balls = pygame.sprite.Group()
-allsprites = pygame.sprite.Group()
-
-play = Player()
-allsprites.add(player)
-
-ball = Ball()
-allsprites.add(ball)
-balls.add(ball)
-
-top = 80
-
-blockcount = 32
-
-c1 = (255, 255, 255)
-c2 = (0, 0, 0)
-c3 = (0, 0, 255)
-
-b_width = 30
-b_height = 30
-
+ 
+black = (255, 255, 255)
+white = (0, 0, 0)
+blue = (0, 0, 255)
+ 
+block_width = 30
+block_height = 30
+ 
 class Block(pygame.sprite.Sprite):
+ 
     def __init__(self, color, x, y):
-        super() .__init__()
-        self.image = pygame.Surface([b_width, b_height])
+        super().__init__()
+        self.image = pygame.Surface([block_width, block_height])
         self.image.fill(color)
-        self.rect = self.image.get_Rect()
+        self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
+ 
+ 
 class Ball(pygame.sprite.Sprite):
     speed = 10.0
     x = 0.0
     y = 180.0
-    dire = 200
-    w = 10
-    h = 10
+    direction = 200
+ 
+    width = 10
+    height = 10
 
-    def __intit__(self):
+    def __init__(self):
         super().__init__()
-        self.image = pygame.Surface([self.w, self.h])
-        self.image.fill(c1)
+
+        self.image = pygame.Surface([self.width, self.height])
+         
+        self.image.fill(white)
+         
         self.rect = self.image.get_rect()
+         
         self.screenheight = pygame.display.get_surface().get_height()
         self.screenwidth = pygame.display.get_surface().get_width()
-
-    def bouncer(self, diff):
-        self.dire = (180 - self.dire) % 360
-        self.dire -= diff
-
-    def update(self):
-        direction_radians = math.radians(self.direction)
+     
+    def bounce(self, diff):
          
+        self.direction = (180 - self.direction) % 360
+        self.direction -= diff
+     
+    def update(self):
+
+        direction_radians = math.radians(self.direction)
+
         self.x += self.speed * math.sin(direction_radians)
         self.y -= self.speed * math.cos(direction_radians)
          
+
         self.rect.x = self.x
         self.rect.y = self.y
          
         if self.y <= 0:
             self.bounce(0)
             self.y = 1
-             
+
         if self.x <= 0:
-            self.dire = (360 - self.dire) % 360
+            self.direction = (360 - self.direction) % 360
             self.x = 1
-             
-        if self.x > self.screenwidth - self.w:
-            self.dire = (360 - self.direction) % 360
-            self.x = self.screenwidth - self.w - 1
-         
+
+        if self.x > self.screenwidth - self.width:
+            self.direction = (360 - self.direction) % 360
+            self.x = self.screenwidth - self.width - 1
+            
         if self.y > 600:
             return True
+        
         else:
             return False
-
+ 
 class Player(pygame.sprite.Sprite):
+     
     def __init__(self):
         super().__init__()
          
@@ -96,7 +87,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.screenheight = pygame.display.get_surface().get_height()
         self.screenwidth = pygame.display.get_surface().get_width()
-
+ 
         self.rect.x = 0
         self.rect.y = self.screenheight-self.height
      
@@ -105,3 +96,87 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = pos[0]
         if self.rect.x > self.screenwidth - self.width:
             self.rect.x = self.screenwidth - self.width
+ 
+pygame.init()
+ 
+screen = pygame.display.set_mode([800, 600])
+ 
+pygame.display.set_caption('Pyblox Game')
+ 
+pygame.mouse.set_visible(0)
+ 
+font = pygame.font.Font(None, 36)
+ 
+background = pygame.Surface(screen.get_size())
+ 
+blocks = pygame.sprite.Group()
+balls = pygame.sprite.Group()
+allsprites = pygame.sprite.Group()
+ 
+player = Player()
+allsprites.add(player)
+ 
+ball = Ball()
+allsprites.add(ball)
+balls.add(ball)
+
+top = 80
+ 
+blockcount = 32
+ 
+ 
+for row in range(5):
+    for column in range(0, blockcount):
+        block = Block(blue, column * (block_width + 2) + 1, top)
+        blocks.add(block)
+        allsprites.add(block)
+    top += block_height + 2
+ 
+clock = pygame.time.Clock()
+game_over = False
+ 
+exit_program = False
+ 
+
+while exit_program != True:
+ 
+    clock.tick(30)
+ 
+    screen.fill(black)
+     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_program = True
+     
+    if not game_over:
+
+        player.update()
+        game_over = ball.update()
+     
+    if game_over:
+        text = font.render("Game Over", True, white)
+        textpos = text.get_rect(centerx=background.get_width()/2)
+        textpos.top = 300
+        screen.blit(text, textpos)
+     
+    if pygame.sprite.spritecollide(player, balls, False):
+
+        diff = (player.rect.x + player.width/2) - (ball.rect.x+ball.width/2)
+
+        ball.rect.y = screen.get_height() - player.rect.height - ball.rect.height - 1
+        ball.bounce(diff)
+     
+    deadblocks = pygame.sprite.spritecollide(ball, blocks, True)
+     
+    if len(deadblocks) > 0:
+        ball.bounce(0)
+         
+
+        if len(blocks) == 0:
+            game_over = True
+
+    allsprites.draw(screen)
+
+    pygame.display.flip()
+ 
+pygame.quit()
